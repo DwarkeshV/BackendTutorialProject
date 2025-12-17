395f7bb72827a4932ae352d7cf127157127e216a
DwarkeshV <vaghasiyadwarkesh2005@gmail.com>
2025-12-17 20:43:16 +0530

    Lern Middleware and Cloudinary link

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,26 @@
+import {v2 as cloudinary} from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+    });
+
+const uploadOnCloudinary= async(localFilePath)=>{
+    try {
+        if(!localFilePath) return null;
+        const response= await cloudinary.uploader.upload(localFilePath,{
+            resource_type:"auto"
+        });
+        console.log("Cloudinary upload response:", response.url);
+        fs.unlinkSync(localFilePath);
+        return response;
+    } catch (error) {
+        console.error("Error uploading to Cloudinary:", error);
+        fs.unlinkSync(localFilePath); // Remove the local file if upload fails
+        return null;
+    }
+}
+
+export {uploadOnCloudinary};
